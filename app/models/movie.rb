404d96b306f4e430db09f18afe80671e21ac1c3d
david <davmwa90@gmail.com>
2023-03-16 16:43:41 +0300

    completed assignment

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ApplicationRecord
+
+validates :title, presence: true
+validates :year, numericality: {
+    greater_than_or_equal_to: 1988, 
+    less_than_or_equal_to: Date.today.year
+}
+validates :poster_url, presence: true
+validates :category, inclusion: {
+in: CATEGORIES, 
+    message: "must be one of: #{CATEGORIES.join(', ')}" 
+}
+
 end
